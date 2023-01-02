f1c446cc549a57950609cdf60d690033b171a8a2
Morfeu12 <omarcosta152@gmail.com>
2023-01-02 17:34:19 -0300

    Flying Felpudo (release init)

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class GameController : MonoBehaviour
     public GameObject enemyPrefab;
     public List<GameObject> enemys = new List<GameObject>();
     private GameObject player;
+    private GameObject playAgainPanel;
     private Text pressStart;
     private Text textScore;
     private int score;
@@ -16,14 +18,12 @@ public class GameController : MonoBehaviour
     private bool gameover = false;
 
     
-    void Start()
-    {
+    void Start() {
         player = GameObject.FindGameObjectWithTag("Player");
+        playAgainPanel = GameObject.Find("Play Again Panel");
         textScore = GameObject.Find("Score").GetComponent<Text>();
         pressStart = GameObject.Find("Press Start").GetComponent<Text>();
         pressStart.text = "Touch for start";
-        // InvokeRepeating("RespawnEnemys", 1.0f, 1.5f);
-        
     }
 
     
@@ -31,10 +31,10 @@ public class GameController : MonoBehaviour
     {
         if(!gameover) {
             CheckPoints();
+        } else if (gameover) {
+            Invoke("PlayAgain", 1.5f);
         }
         CanvasGUI();
-        
-
     }
 
 
@@ -68,12 +68,28 @@ public class GameController : MonoBehaviour
     private void CanvasGUI() {
         if (!gamestart) {
             if(Input.GetButtonDown("Fire1") || Input.GetKeyDown("space") ) {       
-                Destroy(pressStart);
+                playAgainPanel.SetActive(false);
+                pressStart.text = "";
                 InvokeRepeating("RespawnEnemys", 1.0f, 1.5f);
                 gamestart = true;
             }
+        } else {
+            textScore.text = "Score: " + score.ToString();
         }
-        textScore.text = "Score: " + score.ToString();
+        if (gameover) {
+            playAgainPanel.SetActive(true);
+            pressStart.text = "Touch for Play again";
+            pressStart.transform.position = new Vector2(Screen.width/2,Screen.height/2 - 75);
+            textScore.transform.position  = new Vector2(Screen.width/2,Screen.height/2 + 75);
+            textScore.fontSize = 100;
+
+        }
+    }
+
+    private void PlayAgain() {
+        if(Input.GetButtonDown("Fire1") || Input.GetKeyDown("space") ) { 
+            SceneManager.LoadScene("Game", LoadSceneMode.Single);
+        }
     }
 
 
